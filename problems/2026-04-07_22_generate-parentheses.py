dd1083fab978d93a716bd8c00f60261a94289315
esamnyu <es5888@nyu.edu>
2026-04-07 06:11:32 +0000

    feat: Start LeetCode Generate parentheses (2026-04-07)

---
+++ problems/2026-04-07_22_generate-parentheses.py
@@ -0,0 +1,26 @@
+# LeetCode 22: Generate Parentheses
+# Date: 2026-04-07
+# Difficulty: Medium
+# Topics: String, Dynamic Programming, Backtracking
+
+'''
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ 
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+Input: n = 1
+Output: ["()"]
+
+ 
+Constraints:
+
+1 <= n <= 8
+
+
+'''
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
