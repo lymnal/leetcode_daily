7a792cee7d9fa5a6d2fb76b27a0d2e73cb080097
esamnyu <es5888@nyu.edu>
2026-05-09 07:00:43 +0000

    feat: Start LeetCode Minimum length of anagram concatenation (2026-05-09)

---
+++ problems/2026-05-09_3138_minimum-length-of-anagram-concatenation.py
@@ -0,0 +1,41 @@
+# LeetCode 3138: Minimum Length of Anagram Concatenation
+# Date: 2026-05-09
+# Difficulty: Medium
+# Topics: Hash Table, String, Counting
+
+'''
+You are given a string s, which is known to be a concatenation of anagrams of some string t.
+Return the minimum possible length of the string t.
+An anagram is formed by rearranging the letters of a string. For example, "aab", "aba", and, "baa" are anagrams of "aab".
+ 
+Example 1:
+
+Input: s = "abba"
+Output: 2
+Explanation:
+One possible string t could be "ba".
+
+Example 2:
+
+Input: s = "cdef"
+Output: 4
+Explanation:
+One possible string t could be "cdef", notice that t can be equal to s.
+
+Example 2:
+
+Input: s = "abcbcacabbaccba"
+Output: 3
+
+ 
+Constraints:
+
+1 <= s.length <= 105
+s consist only of lowercase English letters.
+
+
+'''
+
+class Solution:
+    def minAnagramLength(self, s: str) -> int:
+        
