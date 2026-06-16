36be5d3dce2221fe858813b5fc3b31e4a5d576f9
esamnyu <es5888@nyu.edu>
2026-06-16 09:59:51 +0000

    feat: Start LeetCode Base 7 (2026-06-16)

---
+++ problems/2026-06-16_504_base-7.py
@@ -0,0 +1,26 @@
+# LeetCode 504: Base 7
+# Date: 2026-06-16
+# Difficulty: Easy
+# Topics: Math, String
+
+'''
+Given an integer num, return a string of its base 7 representation.
+ 
+Example 1:
+Input: num = 100
+Output: "202"
+Example 2:
+Input: num = -7
+Output: "-10"
+
+ 
+Constraints:
+
+-107 <= num <= 107
+
+
+'''
+
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        
