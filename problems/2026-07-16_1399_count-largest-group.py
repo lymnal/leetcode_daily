f21941a703b60f1af71e28b3cacd9cf43cd7941c
GitHub Action <action@github.com>
2026-07-16 02:09:56 +0000

    Daily LeetCode challenge for 2026-07-16

---
+++ problems/2026-07-16_1399_count-largest-group.py
@@ -0,0 +1,35 @@
+# LeetCode 1399: Count Largest Group
+# Date: 2026-07-16
+# Difficulty: Easy
+# Topics: Hash Table, Math, Counting
+
+'''
+You are given an integer n.
+We need to group the numbers from 1 to n according to the sum of its digits. For example, the numbers 14 and 5 belong to the same group, whereas 13 and 3 belong to different groups.
+Return the number of groups that have the largest size, i.e. the maximum number of elements.
+ 
+Example 1:
+
+Input: n = 13
+Output: 4
+Explanation: There are 9 groups in total, they are grouped according sum of its digits of numbers from 1 to 13:
+[1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9].
+There are 4 groups with largest size.
+
+Example 2:
+
+Input: n = 2
+Output: 2
+Explanation: There are 2 groups [1], [2] of size 1.
+
+ 
+Constraints:
+
+1 <= n <= 104
+
+
+'''
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        
