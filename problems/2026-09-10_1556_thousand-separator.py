e3469946a18a6287dda2d4296be04701ef61ee77
esamnyu <es5888@nyu.edu>
2026-09-10 09:14:39 +0000

    feat: Start LeetCode Thousand separator (2026-09-10)

---
+++ problems/2026-09-10_1556_thousand-separator.py
@@ -0,0 +1,29 @@
+# LeetCode 1556: Thousand Separator
+# Date: 2026-09-10
+# Difficulty: Easy
+# Topics: String
+
+'''
+Given an integer n, add a dot (".") as the thousands separator and return it in string format.
+ 
+Example 1:
+
+Input: n = 987
+Output: "987"
+
+Example 2:
+
+Input: n = 1234
+Output: "1.234"
+
+ 
+Constraints:
+
+0 <= n <= 231 - 1
+
+
+'''
+
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        
